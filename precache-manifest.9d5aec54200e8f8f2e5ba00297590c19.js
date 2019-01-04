bafd72b2b2719a281c747a67e0aa729ce89f4b57
Deep Gandhi <dgandhi@gatehousemedia.com>
2019-01-04 01:33:33 -0600

    Updates

---
+++ precache-manifest.9d5aec54200e8f8f2e5ba00297590c19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbb25277d09b235b4359",
+    "url": "/react-crypto-order-book/static/js/runtime~main.dbb25277.js"
+  },
+  {
+    "revision": "d35775242b5b13be5440",
+    "url": "/react-crypto-order-book/static/js/main.d3577524.chunk.js"
+  },
+  {
+    "revision": "b83aab4e6cdb014090f6",
+    "url": "/react-crypto-order-book/static/js/1.b83aab4e.chunk.js"
+  },
+  {
+    "revision": "d35775242b5b13be5440",
+    "url": "/react-crypto-order-book/static/css/main.20c0f0f4.chunk.css"
+  },
+  {
+    "revision": "77fea501839655d8d0e3c5fb86173d6f",
+    "url": "/react-crypto-order-book/index.html"
+  }
+];
